e73f643e4bf4e5d59c99d95e198380539ce39c38
Papa Bosu <ub298@nyu.edu>
2026-04-08 17:04:45 +1000

    002: template

---
+++ template/runner.py
@@ -0,0 +1,12 @@
+
+import template
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    print(f" ############   START: template  ############# ")
+    run()
+    print(f" ############    END: template   ############# ")
